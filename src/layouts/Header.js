c49b3e63b9751562834b7f12f17d8dd721aac47f
sokkunnorn <sokkun.norn2019@gmail.com>
2020-12-11 18:16:07 +0700

    Update class name

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,19 +5,19 @@ import MENU_BUILDER from '../navigation/builders/menu.builders';
 function Header () {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container">
-                    <a class="navbar-brand" href="#">Y4 - Web Develop with Framework</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Y4 - Web Develop with Framework</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ml-auto">
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ml-auto">
                             {
                                 MENU_BUILDER.map((value, index) => {
                                     return (
-                                        <li class="nav-item" key={ index }>
-                                            <a class="nav-link" href={value.part}>{ value.name }</a>
+                                        <li className="nav-item" key={ index }>
+                                            <a className="nav-link" href={value.part}>{ value.name }</a>
                                         </li>
                                     )
                                 })
