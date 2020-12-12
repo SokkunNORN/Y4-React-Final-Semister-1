7413a35fcd3d1f945f24dcdd733aef8acc66c16b
sokkunnorn <sokkun.norn2019@gmail.com>
2020-12-12 15:01:12 +0700

    add 404 page

---
+++ src/views/four0four/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+function Four0Four () {
+    return (
+        <>
+            <div class="page-wrap d-flex flex-row align-items-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 text-center">
+                            <span class="display-1 d-block">404</span>
+                            <h2>Oops!</h2>
+                            <div class="mb-4 lead">
+                                We're sorry, 
+                                <br/>
+                                The page you were looking for doesn't exist anymore.
+                            </div>
+                            <a href="/" class="btn btn-outline-danger">Back to Home</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Four0Four
