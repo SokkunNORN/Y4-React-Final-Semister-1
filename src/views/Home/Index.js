607c238efb8bb93f97ceaa772a4ace6cb026c670
sokkunnorn <sokkun.norn2019@gmail.com>
2020-12-28 20:49:11 +0700

    update mock and label

--- src/views/Home/Index.js
+++ src/views/Home/Index.js
@@ -6,10 +6,10 @@ import listTasks from '../../mock.json'
 function Home () {
     return (
         <div id="home">
-            <div className="row">
+            <div className="d-flex flex-row">
                 {listTasks.data.map(function (item, i) {
                     return (
-                        <div className="col col-sm-12 col-md-6 col-lg-4" key={i}>
+                        <div className="column" key={i}>
                             <Card task={ item } />
                         </div>
                     )
