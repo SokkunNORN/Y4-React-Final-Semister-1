689a8585813a74c05a6ea430205d743c01bc627e
sokkunnorn <sokkun.norn2019@gmail.com>
2020-12-31 13:06:31 +0700

    add scroll

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -2,14 +2,14 @@
 import { RouteWithSubRoutes } from '../router'
 import { Switch } from "react-router-dom";
 
-import Header from './sidebar/Sidebar'
+import Sidebar from './sidebar/Sidebar'
 import './Dashboard.css'
 
 function Dashboard ({ routes }) {
     return (
         <>
             <div className="wrapper d-flex">
-                <Header />
+                <Sidebar />
 
                 <div className="contain">
                     <div className="container-fluid mt-4 pl-4 pr-4">
