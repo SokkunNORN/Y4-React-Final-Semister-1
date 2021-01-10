2d78613d5fdcf8f98bd4683788e849eee627a1bd
sokkunnorn <sokkun.norn2019@gmail.com>
2021-01-10 13:25:08 +0700

    update text

--- src/views/Home/Index.js
+++ src/views/Home/Index.js
@@ -25,7 +25,7 @@ function Home () {
         if (value === dropListBoards[2]) {
             Swal.fire({
                 title: 'Are you sure?',
-                text: "You won't be able to revert board!",
+                text: "You won't be able to revert this board!",
                 icon: 'warning',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
