cb13fa88f12d5ecabd67ed9a09cdb948f499b2e3
sokkun <sokkun.norn2019@gmail.com>
2021-03-02 14:46:22 +0700

    format code

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,14 +1,14 @@
 import firbase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAb_0pyHLFXANXmYHjjWNnQ9OSpIabMx1o",
-    authDomain: "order-nham.firebaseapp.com",
-    projectId: "order-nham",
-    storageBucket: "order-nham.appspot.com",
-    messagingSenderId: "421729946563",
-    appId: "1:421729946563:web:d988769c26235b11db8b76"
-  };
+  apiKey: "AIzaSyAb_0pyHLFXANXmYHjjWNnQ9OSpIabMx1o",
+  authDomain: "order-nham.firebaseapp.com",
+  projectId: "order-nham",
+  storageBucket: "order-nham.appspot.com",
+  messagingSenderId: "421729946563",
+  appId: "1:421729946563:web:d988769c26235b11db8b76"
+};
 
-  const fire = firbase.initializeApp(firebaseConfig);
+const fire = firbase.initializeApp(firebaseConfig);
 
-  export default fire;
+export default fire;
