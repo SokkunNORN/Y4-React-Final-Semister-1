35fe1078206fc120808df700b8ba9f421c863a46
sokkun <sokkun.norn2019@gmail.com>
2021-03-29 21:01:03 +0700

    add role api

---
+++ src/api/role.js
@@ -0,0 +1,45 @@
+import fire from '../config/fire'
+import { handleErrorMassage } from '../mixins/handleMessage'
+import { Role } from '../class/role'
+
+const docRef = fire.firestore().collection("/user-role")
+
+export async function getRole() {
+    var roles = []
+
+    await docRef
+    .get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            const role = new Role(
+                doc.id,
+                doc.data().role,
+                doc.data().description
+            )
+            roles.push(role)
+        })
+    })
+    .catch((error) => {
+        handleErrorMassage("Error getting documents: ", error)
+    });
+
+    return roles
+}
+
+export async function selectRole(id) {
+    console.log('from db: ', id)
+    var role = {}
+
+    await docRef.doc(id)
+    .get().then((doc) => {
+        role = new Role(
+            doc.id,
+            doc.data().role,
+            doc.data().description
+        )
+    })
+    .catch((error) => {
+        handleErrorMassage("Error getting documents: ", error)
+    });
+
+    return role
+}
